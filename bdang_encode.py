3838aa43bd3ffef9e0d58808823b785d1a7f889f
bdang831 <debatebethany@gmail.com>
2023-10-25 22:04:11 -0400

    added comments

--- bdang_encode.py
+++ bdang_encode.py
@@ -4,7 +4,10 @@
 def encode(to_encode):
     encoded = ""
     for i in range(len(to_encode)):
+        # shifts each digit by 3
         current_digit = int(to_encode[i]) + 3
+        # decreases by 10 if greater than ten
+        # ex: 9 + 3 = 12, shifted down 10 to 2
         if current_digit >= 10:
             current_digit -= 10
         encoded += str(current_digit)
@@ -23,6 +26,7 @@ if __name__ == "__main__":
         user_choice = int(input("Please enter an option: "))
         if user_choice == 1:
             string_to_encode = input("Please enter your password to encode: ")
+            # password is saved as encoded password
             password = encode(string_to_encode)
             print("Your password has been encoded and stored!")
 
